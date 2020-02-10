6e6ab2cf4045d8b68882ac194807e62c6be165de
jimmymetts <jimmymetts@bellsouth.net>
2020-02-10 17:17:24 -0600

    added posts & postsFrom components

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './components/Posts';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className='App-title'>Welcome To React</h1>
+        </header>
+    <Posts/>;
+
     </div>
   );
 }

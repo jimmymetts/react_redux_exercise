a10113c3c3fd8626aece8878f048ac47412adeba
jimmymetts <jimmymetts@bellsouth.net>
2020-02-10 18:09:19 -0600

    add inputs and submit button

--- src/components/PostsForm.js
+++ src/components/PostsForm.js
@@ -11,6 +11,19 @@ class PostsForm extends Component {
         return (
             <div>
                 <h1>Add Posts</h1>
+                <form>
+                    <div>
+                        <label>Title:</label> <br/>
+                        <input type='text' name='title'></input>
+                    </div>
+                    <div>
+                        <label>Body:</label> <br/>
+                        <textarea name="body"/>
+                    </div>
+                    <br />
+
+                    <button type="submit">Submit</button>
+                </form>
                
             </div>
         )
